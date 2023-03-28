fd352b4c3aba5165f12dfb4207727309b1ed16e6
jyunhan <jyunhan.liou@gmail.com>
2023-03-28 19:27:58 +0800

    randomly 3-5 sec.

--- src/schedule/index.js
+++ src/schedule/index.js
@@ -1,16 +1,17 @@
 const CronJob = require('cron').CronJob;
 const { resolveQuesetion } = require('../services/question');
 
-const questionResolveTimeInterval = 2; // represent: sec.
-
 const job = new CronJob(
-    `*/${questionResolveTimeInterval} * * * * *`,
-    async () => {
-        await resolveQuesetion();
+    '*/4 * * * * *',
+    () => { // no need async here, it execute in child_process
+        const idleTime = Math.floor(Math.random() * 2);
+
+        // Randomly 3 - 5 sec.,
+        setTimeout(async () => {
+            await resolveQuesetion();
+        }, idleTime * 1000)
     },
     null,
     true,
     'Asia/Taipei'
 );
-
-
